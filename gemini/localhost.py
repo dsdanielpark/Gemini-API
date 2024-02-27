b4adf8a71af0b3248e95aeb0c503c102930ca722
dsdanielpark <parkminwoo1991@gmail.com>
2024-02-27 14:54:12 +0900

    chore: style black

--- gemini/localhost.py
+++ gemini/localhost.py
@@ -6,25 +6,28 @@ import http.server
 import socketserver
 import threading
 
+
 class SimpleHTTPServer:
     """
     A simple HTTP server that serves files from the current directory and below.
-    
+
     Attributes:
         port (int): The port number on which the HTTP server will listen.
         server (socketserver.TCPServer): The server instance.
         thread (threading.Thread): The thread on which the server runs.
     """
-    
+
     def __init__(self, port=8080):
         """
         Initializes the SimpleHTTPServer instance with the given port.
-        
+
         Args:
             port (int): The port number on which the HTTP server will listen. Defaults to 8080.
         """
         self.port = port
-        self.server = socketserver.TCPServer(("localhost", self.port), http.server.SimpleHTTPRequestHandler)
+        self.server = socketserver.TCPServer(
+            ("localhost", self.port), http.server.SimpleHTTPRequestHandler
+        )
         self.thread = threading.Thread(target=self.server.serve_forever)
         self.thread.daemon = True
 
