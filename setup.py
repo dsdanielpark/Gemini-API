d4332cf56c382ed74dd2ba83bd1876c0e1ef0d79
dsdanielpark <parkminwoo1991@gmail.com>
2024-02-27 19:16:06 +0900

    refactor: clean setup

--- setup.py
+++ setup.py
@@ -1,38 +1,27 @@
-# Copyright 2024 Daniel Park, MIT License
 import re
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
-
-def get_version():
-    filename = "gemini/__init__.py"
-    with open(filename) as f:
-        match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""", f.read(), re.M)
-    if not match:
-        raise RuntimeError("{} doesn't contain __version__".format(filename))
-    version = match.groups()[0]
-    return version
-
-
-def get_long_description():
-    with open("README.md", encoding="UTF-8") as f:
-        long_description = f.read()
-        return long_description
-
-
-version = get_version()
+def read(file_path, version=False):
+    with open(file_path, encoding="UTF-8") as f:
+        content = f.read()
+    if version:
+        match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""", content, re.M)
+        if not match:
+            raise RuntimeError(f"{file_path} doesn't contain __version__")
+        return match.group(1)
+    return content
 
 setup(
     name="python-gemini-api",
-    version="0.1.2",
-    author="daniel park",
+    version=read("gemini/__init__.py", version=True),
+    author="Daniel Park",
     author_email="parkminwoo1991@gmail.com",
     description="The python package that returns Response of Google Gemini through API.",
-    long_description=get_long_description(),
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/dsdanielpark/Gemini-API",
     packages=find_packages(exclude=[]),
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=[
         "httpx[http2]>=0.20.0",
         "requests",
@@ -54,11 +43,10 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
