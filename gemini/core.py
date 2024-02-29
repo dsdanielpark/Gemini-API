07dfee5e1cb2ff1bb851afcc7b28925b89969ceb
dsdanielpark <parkminwoo1991@gmail.com>
2024-02-29 21:00:21 +0900

    chore: style black

--- gemini/core.py
+++ gemini/core.py
@@ -9,6 +9,7 @@ from requests.exceptions import RequestException
 
 from .constants import HEADERS, HOST, BOT_SERVER, POST_ENDPOINT
 
+
 class Gemini:
     def __init__(self, cookies: Optional[Dict[str, str]] = None) -> None:
         self.session: requests.Session = requests.Session()
@@ -22,7 +23,9 @@ class Gemini:
             response: requests.Response = self.session.get(f"{self.base_url}/app")
             response.raise_for_status()
         except RequestException as e:
-            raise ConnectionError(f"Failed to connect to {self.base_url}: {str(e)}") from e
+            raise ConnectionError(
+                f"Failed to connect to {self.base_url}: {str(e)}"
+            ) from e
 
         sid: str = self._search_regex(response.text, r'"FdrFJe":"([\d-]+)"', "SID")
         nonce: str = self._search_regex(response.text, r'"SNlM0e":"(.*?)"', "nonce")
@@ -41,19 +44,23 @@ class Gemini:
         return int("".join(random.choices(string.digits, k=7)))
 
     def _construct_params(self, sid: str) -> str:
-        return urllib.parse.urlencode({
-            "bl": BOT_SERVER,
-            "hl": "en",
-            "_reqid": self._get_reqid(),
-            "rt": "c",
-            "f.sid": sid,
-        })
+        return urllib.parse.urlencode(
+            {
+                "bl": BOT_SERVER,
+                "hl": "en",
+                "_reqid": self._get_reqid(),
+                "rt": "c",
+                "f.sid": sid,
+            }
+        )
 
     def _construct_payload(self, prompt: str, nonce: str) -> str:
-        return urllib.parse.urlencode({
-            "at": nonce,
-            "f.req": json.dumps([None, json.dumps([[prompt], None, None])]),
-        })
+        return urllib.parse.urlencode(
+            {
+                "at": nonce,
+                "f.req": json.dumps([None, json.dumps([[prompt], None, None])]),
+            }
+        )
 
     def send_request(self, prompt: str) -> Tuple[str, Optional[int]]:
         try:
