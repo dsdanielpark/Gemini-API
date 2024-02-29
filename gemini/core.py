949c789ea93aa0a7fd5bbd178c6991d983f2880c
dsdanielpark <parkminwoo1991@gmail.com>
2024-02-29 23:36:12 +0900

    feat: add parameter and setter and getter methods

--- gemini/core.py
+++ gemini/core.py
@@ -1,28 +1,113 @@
 # Copyright 2024 Daniel Park, Antonio Cheang, MIT License
-import requests
+
+import os
 import re
+import json
 import random
 import string
-import json
+import requests
 import urllib.parse
 from typing import Optional, Tuple, Dict, Any
 from requests.exceptions import RequestException
 
-from .constants import HEADERS, HOST, BOT_SERVER, POST_ENDPOINT
+from .constants import HEADERS, HOST, BOT_SERVER, POST_ENDPOINT, SUPPORTED_BROWSERS
 
 
 class Gemini:
     def __init__(
-        self, cookies: Optional[Dict[str, str]] = None, cookie_fp: str = None
+        self,
+        session: Optional[requests.Session] = None,
+        cookies: Optional[Dict[str, str]] = None,
+        cookie_fp: str = None,
+        auto_cookies: bool = False,
+        timeout: int = 30,
+        proxies: Optional[dict] = None,
     ) -> None:
+        self.auto_cookies = auto_cookies
+        self.cookies = cookies or {}
+        self._set_cookies(auto_cookies)
+        self.proxies = proxies or {}
+        self.timeout = timeout
+        self.session = self._set_session(session)
         self.session: requests.Session = requests.Session()
         self.base_url: str = HOST
+
         self.session.headers.update(HEADERS)
         if cookies:
             self.session.cookies.update(cookies)
 
         if cookie_fp:
             self._load_cookies_from_file(cookie_fp)
+
+    def _set_cookies(self, auto_cookies: bool) -> None:
+        """
+        Updates the instance's cookies attribute with Gemini API tokens, either from environment variables or by extracting them from the browser, based on the auto_cookies flag.
+
+        Args:
+            auto_cookies (bool): Indicates whether to attempt automatic extraction of tokens from the browser's cookies.
+
+        Raises:
+            Exception: If no cookies are provided through environment variables or cannot be extracted from the browser when auto_cookies is True.
+        """
+        if auto_cookies and not self.cookies:
+            try:
+                self._set_cookies_from_browser()  # Assuming this updates self.cookies directly
+            except Exception as e:
+                raise Exception("Failed to extract cookies from browser.") from e
+        if not auto_cookies and not self.cookies:
+            print(
+                "Cookie loading issue, try setting auto_cookies to True. Restart browser, log out, log in for Gemini Web UI to work. Keep a single browser open."
+            )
+        if not self.cookies:
+            raise Exception(
+                "Gemini cookies must be provided through environment variables or extracted from the browser with auto_cookies enabled."
+            )
+
+    def _set_cookies_from_browser(self) -> None:
+        """
+        Extracts Gemini cookies from web browsers' cookies on the system for a specific domain (".google.com").
+
+        Iterates over supported browsers to add found cookies to the instance's cookie store. Supports multiple browsers and OS.
+
+        Updates `cookies` attribute with found cookies.
+
+        Raises:
+            ValueError: If essential cookies are missing after checking all supported browsers.
+        """
+        for browser_fn in SUPPORTED_BROWSERS:
+            try:
+                print(f"Retrieving cookies from {browser_fn} via browser_cookie3.")
+                cj = browser_fn(domain_name=".google.com")
+                self.cookies.update({cookie.name: cookie.value for cookie in cj})
+            except Exception:
+                continue  # Try the next browser if an exception occurs
+
+        if not self.cookies:
+            raise ValueError(
+                "Failed to get cookies. Ensure 'auto_cookies' is True or manually set 'cookies'."
+            )
+
+    def check_session_cookies(self) -> None:
+        """
+        Prints the session's cookies. Indicates if the session is uninitialized.
+        """
+        if self.session:
+            cookies = self.session.cookies.get_dict()
+            cookies_str = "\n".join(f"{key}: {value}" for key, value in cookies.items())
+            print(f"Session Cookies:\n{cookies_str}")
+        else:
+            print("Session not initialized.")
+
+    def check_session_headers(self) -> None:
+        """
+        Prints the session's headers. Indicates if the session is uninitialized.
+        """
+        if self.session:
+            headers = self.session.headers
+            headers_str = "\n".join(f"{key}: {value}" for key, value in headers.items())
+            print(f"Session Headers:\n{headers_str}")
+        else:
+            print("Session not initialized.")
 
     def _load_cookies_from_file(self, file_path: str) -> None:
         """Loads cookies from a file and updates the session."""
@@ -72,7 +157,7 @@ class Gemini:
         return urllib.parse.urlencode(
             {
                 "bl": BOT_SERVER,
-                "hl": "en",
+                "hl": os.environ.get("GEMINI_LANGUAGE", "en"),
                 "_reqid": self._get_reqid(),
                 "rt": "c",
                 "f.sid": sid,
@@ -95,7 +180,13 @@ class Gemini:
             data = self._construct_payload(
                 f"Provide a written response. {prompt}", nonce
             )
-            response = self.session.post(POST_ENDPOINT, params=params, data=data)
+            response = self.session.post(
+                POST_ENDPOINT,
+                params=params,
+                data=data,
+                timeout=self.timeout,
+                proxies=self.proxies,
+            )
             response.raise_for_status()
             return response.text, response.status_code
         except ConnectionError as e:
