d263609638a0f9aed17b6c18f5ff5bc01fad0f14
dsdanielpark <parkminwoo1991@gmail.com>
2024-03-04 19:53:05 +0900

    build: 1.0.0

--- gemini/__init__.py
+++ gemini/__init__.py
@@ -23,7 +23,7 @@ __all__ = [
     "REPLIT_SUPPORT_PROGRAM_LANGUAGES",
     "Tool",
 ]
-__version__ = "0.1.7"
+__version__ = "1.0.0"
 __author__ = (
     "daniel park <parkminwoo1991@gmail.com>, antonio cheang <teapotv8@proton.me>"
 )
