ca28d4da4a84552cb2936778723e65dd4899dd91
dsdanielpark <parkminwoo1991@gmail.com>
2024-03-05 13:54:56 +0900

    note: add docstring in parsing methods

--- gemini/models/parser/methods.py
+++ gemini/models/parser/methods.py
@@ -1,8 +1,20 @@
 from gemini.models.parser.base import ResponseParser
+from typing import Dict, Any
 
 
 class ParseMethod1(ResponseParser):
-    def parse(self, response_text):
+    def parse(self, response_text: str) -> Dict[str, Any]:
+        """
+        Parses the given response text into a structured format.
+
+        This method processes the input text by filtering and organizing its content based on specific criteria. It looks for items that start with certain prefixes or contain specific substrings, excluding items that are encrypted or represent images. The result is structured into a dictionary with keys for text and choices, where each choice includes a choice ID, text, and links.
+
+        Args:
+            response_text (str): The raw response text to be parsed.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the structured representation of the response text. This includes a general text field and fields for each choice found in the response, each with its own ID, text, and list of links.
+        """
         response_items = response_text.lstrip("')]}'\n\n").split("\n")[1].split("\\")
         response_items = [item for item in response_items if item]
         processed_items = [
@@ -59,7 +71,18 @@ class ParseMethod1(ResponseParser):
 
 
 class ParseMethod2(ResponseParser):
-    def parse(self, response_text):
+    def parse(self, response_text: str) -> Dict[str, Any]:
+        """
+        Parses the given response text into a structured JSON-like format.
+
+        This method initially processes the response string to extract items, focusing on those starting with 'n'. It then organizes these items into a JSON-like structure, grouping them into choices based on their keys and values. The method aims to restructure the response into a more readable and accessible format, with each choice assigned a unique key.
+
+        Args:
+            response_text (str): The raw response text to be parsed.
+
+        Returns:
+            Dict[str, Any]: A dictionary representing the structured version of the response text. This includes a series of choices, each with its own set of key-value pairs derived from the response. If a 'choice01' is present, its 'snippet' is also set as the main text of the response.
+        """
         # Initial processing of the response string
         response_items = response_text.lstrip("')]}'\n\n").split("\n")[1].split("\\")
         response_items = [item for item in response_items if item]
