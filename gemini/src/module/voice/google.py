21e116850c083556a2acaa8c4b408225ba312a3b
dsdanielpark <parkminwoo1991@gmail.com>
2024-03-16 16:52:16 +0900

    feat: add voice modules experiment function

---
+++ gemini/src/module/voice/google.py
@@ -0,0 +1,68 @@
+import os
+from gtts import gTTS
+import speech_recognition as sr
+
+
+def google_tts(text, cache_dir, lang="en"):
+    """
+    Generates and saves a spoken version of the input text using Google's Text-to-Speech API.
+
+    Parameters:
+    - text (str): The text to be converted to speech.
+    - cache_dir (str): Directory where the speech file will be saved.
+    - lang (str): The language of the text (default is English, 'en').
+
+    Returns:
+    - str: The file path of the saved speech file.
+
+    Example:
+    >>> save_path = google_tts("Hello, world!", "./cache", "en")
+    >>> print(save_path)
+    ./cache/tts.mp3
+    """
+    tts = gTTS(text=text, lang=lang)
+    os.makedirs(cache_dir, exist_ok=True)
+    save_path = f"{cache_dir}/tts.mp3"
+    tts.save(save_path)
+    return save_path
+
+
+def speech_to_text(audio_file_path: str, recognizer: str = 'google') -> str:
+    """
+    Converts speech in an audio file to text using various Speech Recognition APIs.
+
+    Args:
+        audio_file_path (str): The file path of the audio file to be transcribed.
+        recognizer (str): The speech recognition service to use. Options include 'google', 'bing', 'google_cloud',
+                          'houndify', 'ibm', 'sphinx', 'wit'. Default is 'google'.
+
+    Returns:
+        str: The text transcription of the audio file. Returns an error message if conversion fails or file format is incompatible.
+
+    Raises:
+        ValueError: If the audio file format is not supported or recognizer is not recognized.
+
+    Example:
+        >>> text = speech_to_text("./cache/audio.wav", recognizer='google')
+        >>> print(text)
+        'Hello, world!'
+    """
+    r = sr.Recognizer()
+    try:
+        with sr.AudioFile(audio_file_path) as source:
+            audio_text = r.listen(source)
+    except (EOFError, ValueError) as e:
+        return f"Error processing audio file: {e}. Ensure file is a compatible format (WAV, AIFF, FLAC)."
+
+    try:
+        text = {
+            'google': lambda audio: r.recognize_google(audio),
+            # Add other recognizers here following the pattern
+        }.get(recognizer, lambda audio: None)(audio_text)
+        
+        if text is None:
+            raise ValueError(f"Recognizer '{recognizer}' is not supported.")
+        
+        return text
+    except Exception as e:
+        return f"Failed to convert speech to text: {e}."
---
+++ gemini/src/module/voice/openai.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from openai import OpenAI
+
+
+def openai_tts(
+    text_input,
+    output_file_name=None,
+    model="tts-1",
+    voice="alloy",
+    stream_to_file=False,
+):
+    """
+    Converts text to speech using OpenAI's API. Depending on the parameters, it can save the output to a file or return the audio content.
+
+    Parameters:
+    - text_input (str): The text to be converted to speech.
+    - output_file_name (str, optional): The name of the output file where the speech audio will be saved if stream_to_file is True. Defaults to None.
+    - model (str, optional): The model to use for text-to-speech conversion. Defaults to "tts-1".
+    - voice (str, optional): The voice model to use. Defaults to "alloy".
+    - stream_to_file (bool, optional): Whether to stream the output directly to a file. If False, returns the audio content. Defaults to True.
+
+    Example:
+    ```
+    # Example usage to save output to a file
+    text_input = "Hello world! This is a streaming test."
+    output_file_name = "output.mp3"
+    openai_tts(text_input, output_file_name=output_file_name, stream_to_file=False)
+
+    # Example usage to get audio content without saving to a file
+    audio_content = openai_tts(text_input, stream_to_file=False)
+    ```
+    """
+    client = OpenAI()
+    response = client.audio.speech.create(model=model, voice=voice, input=text_input)
+
+    if stream_to_file and output_file_name:
+        # Determine the full path for the output file
+        speech_file_path = Path(__file__).parent / output_file_name
+        response.stream_to_file(speech_file_path)
+        print(f"Audio file saved to: {speech_file_path}")
+    else:
+        # Return the audio content directly
+        return response.content
+
+
+def openai_stt(audio_file_path, model="whisper-1", response_format="text"):
+    """
+    Converts speech to text using OpenAI's API. This function opens an audio file, sends it for transcription, and either prints or returns the transcribed text.
+
+    Parameters:
+    - audio_file_path (str): The path to the audio file to be transcribed.
+    - model (str, optional): The model to use for speech-to-text conversion. Defaults to "whisper-1".
+    - response_format (str, optional): The format of the response. Defaults to "text".
+
+    Returns:
+    The transcribed text as a string if response_format is "text".
+
+    Example:
+    ```
+    # Example usage to print transcription
+    audio_file_path = "/path/to/file/audio.mp3"
+    transcribed_text = openai_stt(audio_file_path)
+    print(transcribed_text)
+
+    # Example usage with a different model and response format
+    audio_file_path = "/path/to/file/speech.mp3"
+    transcribed_text = openai_stt(audio_file_path, model="whisper-1", response_format="text")
+    print(transcribed_text)
+    ```
+    """
+    client = OpenAI()
+    with open(audio_file_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            model=model, file=audio_file, response_format=response_format
+        )
+    return transcription.text
